fa05957f588c27fa1d4b0972abf4a38fb116add4
jeremystone <6453521+jeremystone@users.noreply.github.com>
2019-08-06 11:28:30 +0100

    - add mongo

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -22,10 +22,11 @@ object AppDependencies {
 
   val compile = Seq(
     ws,
-    "uk.gov.hmrc"   %% "bootstrap-play-26" % "0.34.0",
-    "uk.gov.hmrc"   %% "domain"            % "5.6.0-play-26",
-    "uk.gov.hmrc"   %% "play-hmrc-api"     % "3.4.0-play-26",
-    "org.typelevel" %% "cats-core"         % "1.6.0"
+    "uk.gov.hmrc"   %% "bootstrap-play-26"    % "0.34.0",
+    "uk.gov.hmrc"   %% "domain"               % "5.6.0-play-26",
+    "uk.gov.hmrc"   %% "play-hmrc-api"        % "3.4.0-play-26",
+    "uk.gov.hmrc"   %% "simple-reactivemongo" % "7.20.0-play-26",
+    "org.typelevel" %% "cats-core"            % "1.6.0"
   )
 
   def test(scope: String = "test, it"): Seq[sbt.ModuleID] = Seq(
@@ -34,7 +35,8 @@ object AppDependencies {
     "org.pegdown"            % "pegdown"             % "1.6.0"             % scope,
     "com.typesafe.play"      %% "play-test"          % PlayVersion.current % scope,
     "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.2"             % scope,
-    "com.github.tomakehurst" % "wiremock"            % "2.22.0"            % scope
+    "uk.gov.hmrc"            %% "reactivemongo-test" % "4.15.0-play-26"    % scope,
+    "com.github.tomakehurst" %  "wiremock"           % "2.22.0"            % scope
   )
 
   // Fixes a transitive dependency clash between wiremock and scalatestplus-play
