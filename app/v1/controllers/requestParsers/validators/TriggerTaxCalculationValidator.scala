57e0940b7925689f9f821c770a8afaa4477b3140
daniel-andrews1994 <38686400+daniel-andrews1994@users.noreply.github.com>
2019-08-14 21:21:43 +0100

    Added request parser and validator

---
+++ app/v1/controllers/requestParsers/validators/TriggerTaxCalculationValidator.scala
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2019 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1.controllers.requestParsers.validators
+
+import config.FixedConfig
+import v1.controllers.requestParsers.validators.validations.{JsonFormatValidation, MtdTaxYearValidation, NinoValidation, TaxYearValidation}
+import v1.models.domain.selfAssessment.TriggerTaxCalculationBody
+import v1.models.errors.{MtdError, RuleIncorrectOrEmptyBodyError}
+import v1.models.requestData.selfAssessment.TriggerTaxCalculationRawData
+
+class TriggerTaxCalculationValidator extends Validator[TriggerTaxCalculationRawData] with FixedConfig {
+
+  private val validationSet = List(parameterFormatValidation, bodyFieldValidator, bodyFieldValidator2)
+
+  private def parameterFormatValidation: TriggerTaxCalculationRawData => List[List[MtdError]] = { data =>
+    List(
+      NinoValidation.validate(data.nino),
+      JsonFormatValidation.validate[TriggerTaxCalculationBody](data.body.json, RuleIncorrectOrEmptyBodyError)
+
+    )
+  }
+
+  private def bodyFieldValidator: TriggerTaxCalculationRawData => List[List[MtdError]] = { data =>
+    val req = data.body.json.as[TriggerTaxCalculationBody]
+    List(
+      TaxYearValidation.validate(req.taxYear)
+    )
+  }
+
+  private def bodyFieldValidator2: TriggerTaxCalculationRawData => List[List[MtdError]] = { data =>
+    val req = data.body.json.as[TriggerTaxCalculationBody]
+    List(
+      MtdTaxYearValidation.validate(req.taxYear, 2018)
+    )
+  }
+
+  override def validate(data: TriggerTaxCalculationRawData): List[MtdError] = {
+    run(validationSet, data).distinct
+  }
+}
