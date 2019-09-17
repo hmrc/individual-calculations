f7425feb3b3dc23e5a683a2b3e4d8feff822b234
lukejones <47660116+BetaDraconis@users.noreply.github.com>
2019-09-17 14:39:06 +0100

    Created working implementation of incomeSourceTypeFilter

--- app/v1/models/response/taxableIncome/selfEmployments/LossClaimsDetail.scala
+++ app/v1/models/response/taxableIncome/selfEmployments/LossClaimsDetail.scala
@@ -15,7 +15,7 @@
  */
 package v1.models.response.taxableIncome.selfEmployments
 import play.api.libs.functional.syntax._
-import play.api.libs.json.{JsPath, JsValue, Json, OWrites, Reads, Writes, __}
+import play.api.libs.json.{JsPath, JsValue, Json, OFormat, OWrites, Reads, Writes, __}
 import utils.NestedJsonReads
 
 case class LossClaimsDetail(
@@ -34,13 +34,24 @@ object LossClaimsDetail extends NestedJsonReads {
   // (can refactor out the filtering function to a single place) or
   // get the reads to peak at each array item ???
 
+  case class TypeWrapper(incomeSourceType: String)
+
+  object TypeWrapper{
+    implicit val formats: OFormat[TypeWrapper] = Json.format[TypeWrapper]
+  }
+
   def superReads[A](path: JsPath)(implicit reads: Reads[A]): Reads[Option[Seq[A]]] = {
     path.readNestedNullable[Seq[JsValue]].map(self => toType[A](self))
   }
 
   def toType[A](seq: Option[Seq[JsValue]])(implicit reads: Reads[A]): Option[Seq[A]] = {
-    val mappedSequence = seq.getOrElse(Seq.empty).flatMap(v => v.asOpt[A])
+    val mappedSequence = seq.getOrElse(Seq.empty).flatMap(v => filterByIncomeSourceType(v).asOpt[A])
     if (mappedSequence.isEmpty) None else Some(mappedSequence)
+  }
+
+  def filterByIncomeSourceType(asd: JsValue): JsValue =  asd.asOpt[TypeWrapper] match {
+    case Some(TypeWrapper(incomeSourceType)) if incomeSourceType == "01" => asd
+    case _ => Json.toJson(None)
   }
 
   implicit val reads: Reads[LossClaimsDetail] = (
