09e0b7f7eb5075332243be16919f12a59a3731fe
lukejones <47660116+BetaDraconis@users.noreply.github.com>
2019-09-18 14:21:56 +0100

    Added temporary comments for conditionalReads.

--- app/v1/models/response/taxableIncome/selfEmployments/LossClaimsDetail.scala
+++ app/v1/models/response/taxableIncome/selfEmployments/LossClaimsDetail.scala
@@ -34,19 +34,24 @@ object LossClaimsDetail extends NestedJsonReads {
 
   implicit val writes: OWrites[LossClaimsDetail] = Json.writes[LossClaimsDetail]
 
-  // TODO either extend each of the array objects with incomeSourceId and incomeSourceType and filter in here
-  // (can refactor out the filtering function to a single place) or
-  // get the reads to peak at each array item ???
-
+  // Each JsArray is read in as an optional sequence of JsValues.
     def conditionalReads[A](path: JsPath)(implicit reads: Reads[A]): Reads[Option[Seq[A]]] = {
       path.readNestedNullable[Seq[JsValue]].map(self => toType[A](self))
     }
 
+  /* For each JsValue in the sequence we check if the incomeSourceType is correct,
+   and then attempt to read it in using the JSON reads for whatever type it should map to.
+   The result is flat mapped to remove the JsValues which could not be mapped
+   to the correct type or didn't have the appropriate incomeSourceType field.
+   The resulting sequence is returned as None if it is empty*/
     def toType[A](seq: Option[Seq[JsValue]])(implicit reads: Reads[A]): Option[Seq[A]] = {
       val mappedSequence = seq.getOrElse(Seq.empty).flatMap(js => filterByIncomeSourceType(js).asOpt[A])
       if (mappedSequence.isEmpty) None else Some(mappedSequence)
     }
 
+  /* The incomeSourceType field is read from the JsValue using the FilterWrapper case class
+  and its' associated reads. If the value is correct, the JsValue is simply returned. If the
+  value is not correct, None is returned instead.*/
     def filterByIncomeSourceType(js: JsValue): JsValue =  js.asOpt[FilterWrapper] match {
       case Some(FilterWrapper(incomeSourceType, _)) if incomeSourceType == "01" => js
       case _ => Json.toJson(None)
