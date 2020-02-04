57661cce653a717c44b5a7e14d317492ef49b7d7
Luke Jones <47660116+BetaDraconis@users.noreply.github.com>
2020-02-04 12:51:18 +0000

    MTDSA-4726: Added logic to map empty LossClaimsSummary objects to None. (#76)
    MTDSA-4726: Added logic to map empty LossClaimsSummary objects to None in JSON reads.

--- app/v1/models/response/getCalculation/taxableIncome/detail/selfEmployment/SelfEmployment.scala
+++ app/v1/models/response/getCalculation/taxableIncome/detail/selfEmployment/SelfEmployment.scala
@@ -44,7 +44,7 @@ object SelfEmployment extends NestedJsonReads {
 
   implicit val writes: OWrites[SelfEmployment] = Json.writes[SelfEmployment]
 
-  private implicit val singleReads: Reads[SelfEmployment] = ((JsPath \ "incomeSourceId").read[String] and
+  implicit val singleReads: Reads[SelfEmployment] = ((JsPath \ "incomeSourceId").read[String] and
     (JsPath \ "totalIncome").readNullable[BigDecimal] and
     (JsPath \ "totalExpenses").readNullable[BigDecimal] and
     (JsPath \ "netProfit").readNullable[BigDecimal] and
@@ -56,7 +56,7 @@ object SelfEmployment extends NestedJsonReads {
     (JsPath \ "adjustedIncomeTaxLoss").readNullable[BigInt] and
     (JsPath \ "taxableProfit").readNullable[BigDecimal] and
     (JsPath \ "taxableProfitAfterIncomeTaxLossesDeduction").readNullable[BigInt] and
-      JsPath.readNullable[LossClaimsSummary].map{
+    JsPath.readNullable[LossClaimsSummary].map{
       case Some(LossClaimsSummary.empty) => None
       case other => other
     } and Reads.pure(None) and Reads.pure(None))(SelfEmployment.apply _)
