d073dd8ca0d15dbc133b59df366a6fcae5dc6724
“Taiwo-AB” <63714619+Taiwo-AB@users.noreply.github.com>
2021-02-01 12:29:16 +0000

    MTDSA-7834 Logging level modifications

--- app/v1/controllers/ListCalculationsController.scala
+++ app/v1/controllers/ListCalculationsController.scala
@@ -79,7 +79,7 @@ class ListCalculationsController @Inject()(
       result.leftMap { errorWrapper =>
         val resCorrelationId = errorWrapper.correlationId
         val result = errorResult(errorWrapper).withApiHeaders(resCorrelationId)
-        logger.info(
+        logger.warn(
           s"[${endpointLogContext.controllerName}][${endpointLogContext.endpointName}] - " +
             s"Error response received with CorrelationId: $resCorrelationId")
         result
